3dfae0d057daa843ef5555a0b7677360b451efd1
vv_tomilin <backatom1@gmail.com>
2022-02-08 18:48:35 +0500

    Clean branch

--- nodejs_intensiv_htmllessons/back/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getUserProfile } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.route('/profile').get(getUserProfile);
-
-export default router;
